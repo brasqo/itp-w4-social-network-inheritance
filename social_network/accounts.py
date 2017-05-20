11bd805091a9e27311ead3eda0d2defa505580d1
brasqo <brasqo@gmail.com>
2017-05-20 12:50:58 -0400

    Update accounts.py

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,13 +1,25 @@
-
 class User(object):
     def __init__(self, first_name, last_name, email):
-        pass
+        # attributes assigned to each individual instance
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.following = []
+        self.posts = []
+
 
     def add_post(self, post):
-        pass
-
+        post.set_user(self)
+        self.posts.append(post) 
+        
     def get_timeline(self):
-        pass
-
+        follower_posts = []
+        for person in self.following:
+            for post in person.posts:
+                follower_posts.append(post)  
+        follower_posts = sorted(follower_posts, key=lambda post: post.timestamp)
+        return follower_posts
+        
     def follow(self, other):
-        pass
+        # following attribute, add to list
+        self.following.append(other)
